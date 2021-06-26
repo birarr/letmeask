51bb756785b057faa409337c00462644b17ca22d
Ubiratan <birarr@gmail.com>
2021-06-26 16:35:43 -0300

    feat: Complete the challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
+import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
